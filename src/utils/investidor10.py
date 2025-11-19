a37e46ca47c028f68f4fe2c89447da85db81077e
Makley <tibolamakley1@gmail.com>
2025-11-18 21:29:00 -0300

    feat: ajusta busca de payout

--- src/utils/investidor10.py
+++ src/utils/investidor10.py
@@ -6,6 +6,7 @@ from src.errors import ElementoNaoEncontradoError
 
 class Investidor10:
   __informacoes_sobre_empresa: List[WebElement] | None = None
+  __indicadores: List[WebElement] | None = None
   def __init__(self, driver: WebDriver):
     self.__driver = driver
 
@@ -18,15 +19,26 @@ class Investidor10:
   def get_dividend_yield(self):
     return self.__driver.find_element(By.CSS_SELECTOR, '#cards-ticker > div._card.dy > div._card-body > span').text.strip()
   def get_payout(self):
-    return self.__driver.find_element(By.CSS_SELECTOR, '#table-indicators > div:nth-child(5) > div.value.d-flex.justify-content-between.align-items-center > span').text.strip()
+    indicatores = self.__scrape_indicadores()
+    for indicador in indicatores:
+      if str(indicador.find_element(By.CSS_SELECTOR, '& > span').text).strip().lower() == 'payout':
+        return indicador.find_element(By.CSS_SELECTOR, 'div.value > span').text.strip()
+
+    raise ElementoNaoEncontradoError(seletor='PAYOUT')
   
-  def get_informacoes_sobre_empresa(self):
+  def __scrape_indicadores(self): 
+    if not self.__indicadores:
+      self.__indicadores = self.__driver.find_elements(By.CSS_SELECTOR, '#table-indicators > div')
+    return self.__indicadores
+
+    
+  def __scrape_informacoes_sobre_empresa(self):
       if not self.__informacoes_sobre_empresa:
         self.__informacoes_sobre_empresa =  self.__driver.find_elements(By.CSS_SELECTOR, '#table-indicators-company > div')
       return self.__informacoes_sobre_empresa
 
   def get_setor(self):
-    informacoes_sobre_empresa = self.get_informacoes_sobre_empresa()
+    informacoes_sobre_empresa = self.__scrape_informacoes_sobre_empresa()
     informacoes_sobre_empresa.reverse()
     
     for elemento in informacoes_sobre_empresa:
@@ -36,7 +48,7 @@ class Investidor10:
     raise ElementoNaoEncontradoError(seletor='SETOR')
   
   def get_segmento_do_setor(self):
-    informacoes_sobre_empresa = self.get_informacoes_sobre_empresa()
+    informacoes_sobre_empresa = self.__scrape_informacoes_sobre_empresa()
     informacoes_sobre_empresa.reverse()
     
     for elemento in informacoes_sobre_empresa:
